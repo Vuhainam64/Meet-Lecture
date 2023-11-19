6ad21233bd1270fb461102d44f0695f621c39b31
Lcd3300 <duylam3300@gmail.com>
2023-11-19 10:38:19 +0700

    xong Search

--- client/src/components/student/Search.jsx
+++ client/src/components/student/Search.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { PiArrowFatLineRightBold } from "react-icons/pi";
 import { getAllUser } from "../../api";
 import { Link } from "react-router-dom";
-export default function Search({chosePage}) {
+export default function Search({ chosePage }) {
   const [searchComponent, setSearchComponent] = useState("");
   const [lecturers, setLecturers] = useState([]);
   const [result, setResult] = useState([]);
@@ -16,32 +16,37 @@ export default function Search({chosePage}) {
   }
   useEffect(() => {
     fetchData();
-    chosePage("Home")
+    chosePage("Home");
   }, [lecturers.length === 0]);
 
   function handleSearching(e) {
     e.preventDefault();
-
+    setSearchComponent(e.target.value);
     setResult(
       lecturers.filter((acc) =>
-        acc.fullname.toLowerCase().includes(searchComponent.toLowerCase())
+        acc.fullname.toLowerCase().includes(e.target.value.toLowerCase())
       )
     );
+  }
+  function searchAll(e) {
+    e.preventDefault();
+    setResult(lecturers);
+    setSearchComponent('')
   }
   return (
     <div className="w-full h-full flex flex-col justify-center items-center gap-5 py-10">
       <form className="flex flex-row justify-between w-1/3">
         <input
           value={searchComponent}
-          onChange={(e) => setSearchComponent(e.target.value)}
+          onChange={(e) => handleSearching(e)}
           placeholder="Lecture's name"
           className="bg-gray-100 placeholder:text-gray-400 w-1/3 rounded-3xl py-3 pl-10 pr-3 placeholder:italic min-w-[15rem]"
         ></input>
         <button
           className="bg-orange-400 text-white rounded-3xl w-fit px-10"
-          onClick={(e) => handleSearching(e)}
+          onClick={(e) => searchAll(e)}
         >
-          Search
+          Search All
         </button>
       </form>
       <div className="w-1/3 h-fit flex flex-col  p-10 border-orange-400 border-4 rounded-md min-h-[25%]">
